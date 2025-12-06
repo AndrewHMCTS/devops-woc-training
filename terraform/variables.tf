fc3e0523428aa7bc5024bd0820a8f5bdad52ee68
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-06 17:53:01 +0000

    pass through env variables into variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,18 @@ variable "aks_name" {
   type    = string
   default = "filevault-aks"
 }
+
+variable "env" {
+  description = "Environment name (sbox, stg, prod)"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
