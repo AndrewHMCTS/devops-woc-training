f4b43cc2e4e4c2a4cbff94cfcbe728bae1353d00
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-09 20:14:39 +0000

    Remove duplicates and add more roles into the locl acr_roles

--- terraform/access_policy.tf
+++ terraform/access_policy.tf
@@ -14,31 +14,14 @@ resource "azurerm_role_assignment" "kv_admin_self" {
   principal_id         = data.azurerm_client_config.current.object_id
 }
 
-resource "azurerm_role_assignment" "webapp_acr_pull" {
-  for_each = local.webapps
-
-  scope                = azurerm_container_registry.acr.id  
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_web_app.apps[each.key].identity[0].principal_id
-
-  depends_on = [azurerm_linux_web_app.apps]
-}
-
-resource "azurerm_role_assignment" "webapp_acr_pull" {
-  for_each = local.webapps
-
-  scope                = azurerm_container_registry.acr.id  
-  role_definition_name = "AcrPull"
-  principal_id         = data.azurerm_client_config.current.object_id
-
-  depends_on = [azurerm_linux_web_app.apps]
-}
-
 locals {
   acr_roles = [
     "Container Registry Repository Writer",
     "Container Registry Repository Reader",
-    "Container Registry Contributor and Data Access Configuration Administrator"
+    "Container Registry Contributor and Data Access Configuration Administrator",
+    "AcrPull",
+    "AcrPush",
+    "SQL DB Contributor"
   ]
 }
 
@@ -66,14 +49,6 @@ resource "azurerm_role_assignment" "sp_acr_roles" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = each.value
   principal_id         = data.azurerm_client_config.current.object_id
-}
-
-resource "azurerm_role_assignment" "webapp_sql_contributor" {
-  for_each = local.webapps
-
-  scope                = azurerm_mssql_server.sql.id 
-  role_definition_name = "SQL DB Contributor"
-  principal_id         = azurerm_linux_web_app.apps[each.key].identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "sp_webapp_contributor" {
