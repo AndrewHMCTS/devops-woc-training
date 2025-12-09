5a498bb934f6efcf6c8a7571ed2ec7d73450522a
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-09 08:46:43 +0000

    add zone to tf code for postgres server

--- src/backend/app/database.py
+++ src/backend/app/database.py
@@ -6,7 +6,7 @@ import os
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_PORT = os.getenv("DB_PORT") or "5432"
 DB_NAME = os.getenv("DB_NAME")
 
 if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_NAME]):
