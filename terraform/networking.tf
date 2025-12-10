358428176c426738fb0b7d807a8c388d51e7773c
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-10 17:39:11 +0000

    update networking to use delegated subnet ips between resources

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -30,7 +30,7 @@ resource "azurerm_subnet" "snet_pe" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.Storage"]
+  service_endpoints    = ["Microsoft.Storage", "Microsoft.KeyVault"]
 
   delegation {
     name = "fs"
