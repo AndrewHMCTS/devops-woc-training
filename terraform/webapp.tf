50489eab6ddf13817359bb15c1bb3f359468672f
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-13 15:03:10 +0000

    add in the backend url via terraform. test deployment and that data persists

--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -34,30 +34,26 @@ resource "azurerm_linux_web_app" "apps" {
     health_check_eviction_time_in_min       = 10
   }
 
-  app_settings = {
-    AZURE_STORAGE_ACCOUNT_NAME = azurerm_key_vault_secret.storage_account_name.value
-    AZURE_STORAGE_ACCOUNT_KEY  = azurerm_key_vault_secret.storage_account_key.value
-    AZURE_CONTAINER_NAME       = azurerm_key_vault_secret.container_name.value
-    DB_USER                    = azurerm_key_vault_secret.pg_user.value
-    DB_PASSWORD                = azurerm_key_vault_secret.pg_password.value
-    DB_HOST                    = azurerm_key_vault_secret.db_host.value
-    DB_NAME                    = azurerm_key_vault_secret.db_name.value
-    DB_PORT                    = azurerm_key_vault_secret.db_port.value
-    DATABASE_URL               = azurerm_key_vault_secret.database_url.value
-    # AZURE_STORAGE_ACCOUNT_NAME = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storage_account_name.id})"
-    # AZURE_STORAGE_ACCOUNT_KEY  = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storage_account_key.id})"
-    # AZURE_CONTAINER_NAME       = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.container_name.id})"
-    # DB_USER                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.pg_user.id})"
-    # DB_PASSWORD                = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.pg_password.id})"
-    # DB_HOST                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_host.id})"
-    # DB_NAME                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_name.id})"
-    # DB_PORT                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_port.id})"
-    # DATABASE_URL               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.database_url.id})"
-    SECRET_KEY                        = azurerm_key_vault_secret.secret_key.value
-    WEBSITES_PORT                     = each.value.port
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
-    DOCKER_ENABLE_CI                  = "true"
-  }
+  app_settings = merge(
+    {
+      AZURE_STORAGE_ACCOUNT_NAME          = azurerm_key_vault_secret.storage_account_name.value
+      AZURE_STORAGE_ACCOUNT_KEY           = azurerm_key_vault_secret.storage_account_key.value
+      AZURE_CONTAINER_NAME                = azurerm_key_vault_secret.container_name.value
+      DB_USER                             = azurerm_key_vault_secret.pg_user.value
+      DB_PASSWORD                         = azurerm_key_vault_secret.pg_password.value
+      DB_HOST                             = azurerm_key_vault_secret.db_host.value
+      DB_NAME                             = azurerm_key_vault_secret.db_name.value
+      DB_PORT                             = azurerm_key_vault_secret.db_port.value
+      DATABASE_URL                        = azurerm_key_vault_secret.database_url.value
+      SECRET_KEY                          = azurerm_key_vault_secret.secret_key.value
+      WEBSITES_PORT                       = each.value.port
+      WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+      DOCKER_ENABLE_CI                    = "true"
+    },
+    each.key == "frontend" ? {
+      BACKEND_URL = "https://${local.webapps["backend"].name}.azurewebsites.net"
+    } : {}
+  )
 
   lifecycle {
     ignore_changes = [
@@ -65,12 +61,3 @@ resource "azurerm_linux_web_app" "apps" {
     ]
   }
 }
-
-# resource "azurerm_app_service_virtual_network_swift_connection" "apps_vnet" {
-#   for_each = azurerm_linux_web_app.apps
-
-#   app_service_id = each.value.id
-#   subnet_id      = azurerm_subnet.snet_appservice.id
-
-#   depends_on = [azurerm_linux_web_app.apps]
-# }
