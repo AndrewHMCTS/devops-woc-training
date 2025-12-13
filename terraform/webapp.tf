53c7cf23d982da6fefbeb69107fd46cbfc33fbe4
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-13 11:43:18 +0000

    inject literal secret values from kv not the reference of the value

--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -33,15 +33,24 @@ resource "azurerm_linux_web_app" "apps" {
   }
 
   app_settings = {
-    AZURE_STORAGE_ACCOUNT_NAME = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storage_account_name.id})"
-    AZURE_STORAGE_ACCOUNT_KEY  = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storage_account_key.id})"
-    AZURE_CONTAINER_NAME       = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.container_name.id})"
-    DB_USER                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.pg_user.id})"
-    DB_PASSWORD                = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.pg_password.id})"
-    DB_HOST                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_host.id})"
-    DB_NAME                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_name.id})"
-    DB_PORT                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_port.id})"
-    DATABASE_URL               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.database_url.id})"
+    AZURE_STORAGE_ACCOUNT_NAME = azurerm_key_vault_secret.storage_account_name.value
+    AZURE_STORAGE_ACCOUNT_KEY  = azurerm_key_vault_secret.storage_account_key.value
+    AZURE_CONTAINER_NAME       = azurerm_key_vault_secret.container_name.value
+    DB_USER                    = azurerm_key_vault_secret.pg_user.value
+    DB_PASSWORD                = azurerm_key_vault_secret.pg_password.value
+    DB_HOST                    = azurerm_key_vault_secret.db_host.value
+    DB_NAME                    = azurerm_key_vault_secret.db_name.value
+    DB_PORT                    = azurerm_key_vault_secret.db_port.value
+    DATABASE_URL               = azurerm_key_vault_secret.database_url.value
+    # AZURE_STORAGE_ACCOUNT_NAME = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storage_account_name.id})"
+    # AZURE_STORAGE_ACCOUNT_KEY  = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storage_account_key.id})"
+    # AZURE_CONTAINER_NAME       = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.container_name.id})"
+    # DB_USER                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.pg_user.id})"
+    # DB_PASSWORD                = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.pg_password.id})"
+    # DB_HOST                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_host.id})"
+    # DB_NAME                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_name.id})"
+    # DB_PORT                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.db_port.id})"
+    # DATABASE_URL               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.database_url.id})"
     SECRET_KEY                 = azurerm_key_vault_secret.secret_key.value
     WEBSITES_PORT              = each.value.port
   }
