75a44a99bf0dbdec39dd80fa0ed8282345732688
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-23 13:43:46 +0000

    comment our environment and location

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
-variable "location" {
-  type    = string
-  default = "uksouth"
-}
+# variable "location" {
+#   type    = string
+#   default = "uksouth"
+# }
 
-variable "env" {
-  description = "Environment name (sbox, stg, prod)"
-  type        = string
-}
+# variable "env" {
+#   description = "Environment name (sbox, stg, prod)"
+#   type        = string
+# }
 
 variable "subscription_id" {
   description = "Azure Subscription ID"
