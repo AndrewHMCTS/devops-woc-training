8ce0b7766406af9d3f11ed6de9c18c61a54d3a03
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-12-23 13:40:33 +0000

    fix variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,27 @@
-# variable "location" {
-#   type    = string
-#   default = "uksouth"
-# }
+variable "location" {
+  type    = string
+  default = "uksouth"
+}
 
-# variable "env" {
-#   description = "Environment name (sbox, stg, prod)"
+variable "env" {
+  description = "Environment name (sbox, stg, prod)"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  default     = ""
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+  default     = ""
+}
+
+# variable "alert_email" {
+#   description = "Email for alert notifications"
 #   type        = string
+#   default     = "andrew.mcdevitt@solirius.com"
 # }
-
-# variable "subscription_id" {
-#   description = "Azure Subscription ID"
-#   type        = string
-#   default     = ""
-# }
-
-# variable "client_id" {
-#   description = "Azure Client ID"
-#   type        = string
-#   default     = ""
-# }
-
-# # variable "alert_email" {
-# #   description = "Email for alert notifications"
-# #   type        = string
-# #   default     = "andrew.mcdevitt@solirius.com"
-# # }
